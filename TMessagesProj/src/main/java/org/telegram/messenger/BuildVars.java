699f76fedaf2754156dde35e503c6ba321cc9dc7
Dmitry Samoylenko <samoylenkodmitry@gmail.com>
2024-10-28 21:06:14 +0300

    app id
    (cherry picked from commit 0d3ebd90f97d2ef36378257c3f0a47931380508f)

--- TMessagesProj/src/main/java/org/telegram/messenger/BuildVars.java
+++ TMessagesProj/src/main/java/org/telegram/messenger/BuildVars.java
@@ -25,8 +25,8 @@ public class BuildVars {
     public static boolean CHECK_UPDATES = true;
     public static boolean NO_SCOPED_STORAGE = Build.VERSION.SDK_INT <= 29;
     public static String BUILD_VERSION_STRING = BuildConfig.BUILD_VERSION_STRING;
-    public static int APP_ID = 4;
-    public static String APP_HASH = "014b35b6184100b085b0d0572f9b5103";
+    public static int APP_ID = 29277708;
+    public static String APP_HASH = "7f2d6e2b6a15092aef88e22fa654cd01";
 
     // SafetyNet key for Google Identity SDK, set it to empty to disable
     public static String SAFETYNET_KEY = "AIzaSyDqt8P-7F7CPCseMkOiVRgb1LY8RN1bvH8";
